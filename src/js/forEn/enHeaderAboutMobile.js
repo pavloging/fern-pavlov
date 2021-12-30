12a1102f45f7d9f03781928f0c3fbf0436c7aa59
Egor Pavlov <73013930+pavlovsun@users.noreply.github.com>
2021-12-30 20:44:43 +1000

    Add files via upload

---
+++ src/js/forEn/enHeaderAboutMobile.js
@@ -0,0 +1,37 @@
+//Header for mobile
+const header = document.querySelector("#header-fern-mobile");
+const html = `
+    <div id="header-fern" data-aos="zoom-in">
+        <img id="header-fern-bg" src="images/mobile/fern.jpg">
+        <h1  onclick="document.location='index.html'" id="header-fern-h1">IP PAVLOVA</h1>
+        <details id="lang">
+            <summary>Lang</summary>
+            <a href="about.html">En</a><br>
+            <a href="ru/about.html">Ru</a>
+        </details>
+        <a href="https://wa.me/79098410641/?text" target="_blank">
+            <img id="header-fern-whatsapp" src="images/other/whatsapp.png">
+        </a>
+        <img id="header-fern-wechat" onclick='wechatOpen()' src="images/other/wechat.png">
+    </div>`;
+header.innerHTML += html;
+
+//WeChat
+function wechatOpen() {
+  let wechatHTML = `
+          <div class="hide-menu">
+              <img id="close" onclick='wechatClose()' src="images/other/close.png">
+              <div id="teg-wechat"><h1>WeChat ID: FernRussia</h1></div>
+              <img id="img-wechat" src="images/other/qr-code.png">
+          </div>`;
+  document.querySelector("#wechat").innerHTML = wechatHTML;
+}
+function wechatClose() {
+  wechatHTML = `
+          <div class="hide-menu" hidden>
+              <img id="close" onclick='wechatClose()' src="images/other/close.png">
+              <div id="teg-wechat"><h1>WeChat ID: FernRussia</h1></div>
+              <img id="img-wechat" src="images/other/qr-code.png">
+          </div>`;
+  document.querySelector("#wechat").innerHTML = wechatHTML;
+}
